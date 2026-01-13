0f0535f3954dff4cad67f1827c03382718c0c509
HANDZCZ <handz@email.cz>
2026-01-13 12:54:55 +0100

    Add nix-community cache

--- flake.nix
+++ flake.nix
@@ -40,9 +40,11 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://nix-community.cachix.org"
       "https://attic.xuyh0120.win/lantian" # nix-cachyos-kernel
     ];
     extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" # nix-cachyos-kernel
     ];
   };
