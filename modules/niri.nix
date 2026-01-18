d5c5d5400cb5e829fdd2539e294066456789189d
HANDZCZ <handz@email.cz>
2026-01-18 01:04:52 +0100

    Fix steam crashing xwayland-satellite

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,8 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
 {
   environment.systemPackages = with pkgs; [
-    xwayland-satellite
+    # TODO: change back to stable once version 0.8.0 hits stable
+    # fixes: steam crashing on startup, and bringing down with it all other apps dependent on xwayland-sattelite
+    #        https://github.com/Supreeeme/xwayland-satellite/issues/282
+    #        https://discourse.nixos.org/t/steam-not-launching-in-25-11/73536
+    pkgs-unstable.xwayland-satellite
     alacritty
     swayidle
     wev
