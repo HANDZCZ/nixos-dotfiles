2d7a62d4438e74ea1ba777dea705f9a94311f268
HANDZCZ <handz@email.cz>
2026-01-21 10:24:04 +0100

    Add ranger

--- users/handz/home.nix
+++ users/handz/home.nix
@@ -27,6 +27,25 @@
     TERMINAL = "alacritty";
   };
 
+  programs.ranger = {
+    enable = true;
+    settings = {
+      show_hidden = true;
+    };
+    extraConfig = ''
+      default_linemode devicons
+    '';
+    plugins = [
+      {
+        name = "ranger_devicons";
+        src = builtins.fetchGit {
+          url = "https://github.com/alexanderjeurissen/ranger_devicons.git";
+          rev = "1bcaff0366a9d345313dc5af14002cfdcddabb82";
+        };
+      }
+    ];
+  };
+
   programs.lazygit = {
     enable = true;
     settings = {
