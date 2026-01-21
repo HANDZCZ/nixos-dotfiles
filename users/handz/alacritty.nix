aa6f21aa70d9da888ff0659afb9f48d8d4165b1c
HANDZCZ <handz@email.cz>
2026-01-21 10:25:21 +0100

    Fix which-key not working with numpad numbers

--- users/handz/alacritty.nix
+++ users/handz/alacritty.nix
@@ -8,6 +8,20 @@
         padding = { x = 5; y = 5; };
         opacity = 0.8;
       };
+      keyboard = {
+        bindings = [
+          { key = "Numpad0"; chars = "0"; }
+          { key = "Numpad1"; chars = "1"; }
+          { key = "Numpad2"; chars = "2"; }
+          { key = "Numpad3"; chars = "3"; }
+          { key = "Numpad4"; chars = "4"; }
+          { key = "Numpad5"; chars = "5"; }
+          { key = "Numpad6"; chars = "6"; }
+          { key = "Numpad7"; chars = "7"; }
+          { key = "Numpad8"; chars = "8"; }
+          { key = "Numpad9"; chars = "9"; }
+        ];
+      };
     };
   };
 }
