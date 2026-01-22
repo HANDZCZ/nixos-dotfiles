3e19e17626b52e4564278e08dbcb9e2585a8f1ca
HANDZCZ <handz@email.cz>
2026-01-22 14:47:48 +0100

    Edit nixvim config
    Set colorscheme to darker
    Replace lightline with lualine

--- users/handz/nixvim.nix
+++ users/handz/nixvim.nix
@@ -18,7 +18,12 @@ in {
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
-    colorschemes.onedark.enable = true;
+    colorschemes.onedark = {
+      enable = true;
+      settings = {
+        style = "darker";
+      };
+    };
     globals = {
       mapleader = " ";
       markdown_folding = 1;
@@ -69,9 +74,38 @@ in {
       rnvimr
     ];
     plugins = {
-      lightline = {
+      lualine = {
         enable = true;
-        settings.colorscheme = "one";
+        settings = {
+          tabline = {
+            lualine_a = [
+              {
+                __unkeyed-1 = "tabs";
+                show_modified_status = false;
+                use_mode_colors = true;
+                max_length = { __raw = "vim.o.columns - 50"; };
+                mode = 1;
+                path = 0;
+                fmt = { __raw = ''
+                  function(name, context)
+                    local buflist = vim.fn.tabpagebuflist(context.tabnr)
+                    local winnr = vim.fn.tabpagewinnr(context.tabnr)
+                    local bufnr = buflist[winnr]
+                    local mod = vim.fn.getbufvar(bufnr, "&mod")
+
+                    return name .. (mod == 1 and " +" or "")
+                  end'';
+                };
+              }
+            ];
+            lualine_z = [
+              {
+                __unkeyed-1 = "datetime";
+                style = "default";
+              }
+            ];
+          };
+        };
       };
       nix.enable = true;
       cmp =
