2cf406e295d820732a69e2a7d2fb19f67fc03fce
HANDZCZ <handz@email.cz>
2026-02-04 19:33:45 +0100

    Add plugins to obs

--- users/handz/home.nix
+++ users/handz/home.nix
@@ -50,7 +50,13 @@
 
   programs.obs-studio = {
     enable = true;
-    plugins = with pkgs; [];
+    plugins = with pkgs.obs-studio-plugins; [
+      # no wayland support for now
+      #input-overlay
+      obs-dvd-screensaver
+      obs-pipewire-audio-capture
+      wlrobs
+    ];
     package = pkgs.obs-studio.override {
       cudaSupport = osConfig.hardware.nvidia.enabled;
     };
