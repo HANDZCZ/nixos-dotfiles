e93e2d5636770c03a4823d48c27b8cf8d23f462e
HANDZCZ <handz@email.cz>
2026-02-05 13:09:08 +0100

    Fix icon theme not set in qt

--- users/handz/cursor_icon_themes.nix
+++ users/handz/cursor_icon_themes.nix
@@ -1,20 +1,43 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
-{
+let
+  cursor_theme = {
+    name = "Qogir";
+    package = pkgs.qogir-icon-theme;
+    size = 24;
+  };
+  icon_theme = {
+    name = "Qogir";
+    package = pkgs.qogir-icon-theme;
+  };
+in {
   home.pointerCursor = {
     enable = true;
     gtk.enable = true;
     x11.enable = true;
-    name = "Qogir";
-    size = 24;
-    package = pkgs.qogir-icon-theme;
+    name = cursor_theme.name;
+    size = cursor_theme.size;
+    package = cursor_theme.package;
   };
 
   gtk = {
     enable = true;
     iconTheme = {
-      name = "Qogir";
-      package = pkgs.qogir-icon-theme;
+      name = icon_theme.name;
+      package = icon_theme.package;
     };
   };
+
+  qt =
+    let
+      settingsVersions = [ "5" "6" ];
+      qtConfigFn = version: {
+        Appearance = {
+          icon_theme = icon_theme.name;
+        };
+      };
+    in {
+      enable = true;
+      platformTheme.name = "qtct";
+    } // lib.genAttrs' settingsVersions (version: lib.nameValuePair ("qt" + version + "ctSettings") (qtConfigFn version));
 }
