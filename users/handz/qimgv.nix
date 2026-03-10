e12179c56fced949821c7fac9438656e26e634a8
HANDZCZ <handz@email.cz>
2026-03-10 17:14:58 +0100

    Add patch for qimgv to fix system theme usage

--- users/handz/qimgv.nix
+++ users/handz/qimgv.nix
@@ -1,7 +1,18 @@
-{ lib, pkgs-unstable, ... }:
+{ lib, pkgs, pkgs-unstable, ... }:
 
-{
-  home.packages = [ pkgs-unstable.qimgv ];
+let
+  # use unstable package for Qt6 and more image formats support
+  package = pkgs-unstable.qimgv.overrideAttrs (old: {
+    patches = (old.patches or []) ++ [
+      (pkgs.fetchpatch2 {
+        name = "Fix-system-theme-usage.diff";
+        url = "https://github.com/easymodo/qimgv/pull/639.diff?full_index=1";
+        hash = "sha256-Vvd7hjBs5YZ9uILtvs3CTkh9I823pMyMWNuVOe5u8d0=";
+      })
+    ];
+  });
+in {
+  home.packages = [ package ];
 
   xdg.mimeApps.defaultApplications = lib.genAttrs [
     "image/png"
