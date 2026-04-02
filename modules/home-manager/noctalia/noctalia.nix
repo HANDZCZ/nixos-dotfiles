75727da3c8aab0d85e7b9c58b8f0c88fcd6d0cb7
HANDZCZ <handz@email.cz>
2026-04-02 15:48:22 +0200

    Update noctalia to v4.7.3

--- modules/home-manager/noctalia/noctalia.nix
+++ modules/home-manager/noctalia/noctalia.nix
@@ -1,11 +1,6 @@
 { pkgs, lib, config, inputs, ... }:
 
 let
-  quickshell_patches = [
-    # https://github.com/NixOS/nixpkgs/pull/483139
-    ../../../patches/noctalia-qs/Fix-unneccessary-reloads.patch
-  ];
-
   cfg = config.programs.noctalia-shell;
   overlayType = lib.mkOptionType {
     name = "nixpkgs-overlay";
@@ -61,13 +56,9 @@ in {
 
     final_package = package.override (old-noct: {
       extraPackages = (old-noct.extraPackages or []) ++ cfg.extraPackages;
-      quickshell = old-noct.quickshell.overrideAttrs (old-qs: {
-        patches = (old-qs.patches or []) ++ quickshell_patches;
-      });
     });
   in {
     systemd.enable = lib.mkDefault true;
     package = lib.mkForce final_package;
-    extraPackages = lib.optional (lib.attrByPath [ "appLauncher" "useApp2Unit" ] false cfg.settings) pkgs.app2unit;
   };
 }
